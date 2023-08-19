3e43653a6707bae30be1b76a337902b014cb5039
YaKartoshka <erasyl.zarubekovv@gmail.com>
2023-08-20 02:03:50 +0600

    index updated

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/login', (req,res)=>{
+    res.render('login')
+})
+
 router.get('/', (req,res)=>{
     res.render('index')
 })
+
 
 
 
