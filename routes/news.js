8de250a03de577fb7d96fa958f387753c35b42d2
YaKartoshka <erasyl.zarubekovv@gmail.com>
2023-08-27 15:41:52 +0600

    ambiance and sweetalert added

--- routes/news.js
+++ routes/news.js
@@ -5,8 +5,7 @@ const admin = require('firebase-admin');
 const fs = require('fs');
 const { fdb } = require("../libs/firebase_db");
 const storage=admin.storage().bucket('gs://coach-mate-b8795.appspot.com')
-var r = { r: 200 };
-
+const uuid=require('uuid-v4');
 const multer_storage = multer.diskStorage({
     destination(req, file, cb) {
         cb(null, conf.temp_path);
@@ -25,17 +24,51 @@ const upload = multer({
     limits: limits
 });
 
+const metadata = {
+    metadata: {
+      firebaseStorageDownloadTokens: uuid()
+    },
+    contentType: 'image/png',
+    cacheControl: 'public, max-age=31536000',
+  };
+
 
 
 router.post('/create', upload.single('img'), async function (req, res, next) {
-    console.log(req.file);
-    console.log(req.body)
+    var r = { r: 1 };
+    const image = req.file
+    const panel_id = req.session.panel_id;
+
     var data = {
         title: req.body.title,
         text: req.body.text
     }
+    try{
+        const newsRef = fdb.collection('panels').doc(panel_id).collection('news');
+        const news = await newsRef.add(data);
 
-     const news = fdb.collection('panels').doc(panel_id)
+        await storage.upload(image.path, {
+            gzip: true,
+            metadata: metadata,
+            destination: `news/${image.originalname}`
+        });
+
+
+        var news_image_url=`https://firebasestorage.googleapis.com/v0/b/coach-mate-b8795.appspot.com/o/news%2F${image.originalname}?alt=media`
+        console.log(news.id, news_image_url)
+        await newsRef.doc(news.id).update({ news_id: news.id, news_image: news_image_url });
+        res.send(JSON.stringify(r));
+        fs.unlink(image.path, function (err) {
+            if (err) {
+              console.error(err);
+            }
+          });
+    } catch(e){
+        r['r']=0;
+        res.send(JSON.stringify(r))
+    }
+   
+
 });
 
 
