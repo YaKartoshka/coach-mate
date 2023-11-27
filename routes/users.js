be89d55736328f25dc1f95897431bd06d4158de2
YaKartoshka <erasyl.zarubekovv@gmail.com>
2023-11-28 03:02:47 +0600

    add member, show member and structure modified

--- routes/users.js
+++ routes/users.js
@@ -3,48 +3,53 @@ const validator = require('validator');
 const router = express.Router();
 const { fdb, admin_fauth } = require("../libs/firebase_db");
 
-router.post('/add', async (req, res) => {
+router.post('/create', async (req, res) => {
     var r = { r: 0 };
-    const fieldsToCheck = ['email', 'first_name', 'last_name', 'password', 'phone_number', 'birth_date'];
+    const fieldsToCheck = ['email', 'first_name', 'last_name', 'role'];
     const errors = fieldsToCheck.filter(field => !req.body[field] || req.body[field].trim() === '');
 
     if (errors.length > 0) {
         r['r'] = 3; // invalid or empty fields
         res.send(JSON.stringify(r));
+        return;
     }
 
     if (!validator.isEmail(req.body.email)) {
         r['r'] = 2; // invalid email
         res.send(JSON.stringify(r));
+        return;
     }
 
     const email = req.body.email;
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
-    const password = req.body.password;
-    const phone_number = req.body.phone_number;
-    const birth_date = req.body.birth_date;
+    const password = `${first_name.toLowerCase()}123456`
+    const role = req.body.role;
+
 
     await admin_fauth.createUser({
         email: email,
         emailVerified: true,
-        phoneNumber: phone_number,
         password: password,
         displayName: `${first_name} ${last_name}`,
     }).then(async (userRecord) => {
-        await fdb.collection('panels').doc(req.session.panel_id).collection('users').add({
+        await fdb.collection('panels').doc(req.session.panel_id).collection('users').doc(userRecord.uid).set({
             email: email,
-            phone_number: phone_number,
             first_name: first_name,
             last_name: last_name,
-            birth_date: birth_date,
-            auth_id: userRecord.uid
+            role: role,
+            user_id: userRecord.uid,
+            profile_img: ''
         }).then(() => {
             r['r'] = 1;
             res.send(JSON.stringify(r));
         });
-    }).catch((e) => {
-        console.log(e);
+    }).catch((err) => {
+
+        if (err.code == 'auth/email-already-exists') {
+            r['r'] = 4;
+        }
+        console.log(err);
         res.send(JSON.stringify(r));
     });
 });
@@ -80,7 +85,7 @@ router.post('/edit', async (req, res) => {
     }).then(() => {
         r['r'] = 1;
         res.send(JSON.stringify(r));
-    }).catch((e)=>{
+    }).catch((e) => {
         console.log(e);
         res.send(JSON.stringify(r));
     });
@@ -97,13 +102,35 @@ router.get('/get-all', async (req, res) => {
                     first_name: u_doc.data().first_name,
                     last_name: u_doc.data().last_name,
                     email: u_doc.data().email,
-                    birth_date: u_doc.data().birth_date
+                    role: u_doc.data().role,
+                    profile_img: u_doc.data().profile_img,
                 });
             });
             res.send(JSON.stringify(data));
         });
     } catch (e) {
-        var r ={r:0}; 
+        var r = { r: 0 };
+        res.send(JSON.stringify(r));
+    }
+});
+
+router.get('/get', async (req, res) => {
+    const panel_id = req.session.panel_id;
+    const user_id = req.session.user_id;
+
+    try {
+        await fdb.collection('panels').doc(panel_id).collection('users').doc(user_id).get().then((user) => {
+            let data = {
+                email: user.data().email,
+                first_name: user.data().first_name,
+                last_name: user.data().last_name,
+                profile_img: user.data().profile_img,
+                role: role
+            }
+            res.send(JSON.stringify(data));
+        });
+    } catch (e) {
+        var r = { r: 0 };
         res.send(JSON.stringify(r));
     }
 });
