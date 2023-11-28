737f212871834860eda312769b5fd5de4d443ce7
YaKartoshka <erasyl.zarubekovv@gmail.com>
2023-11-28 23:54:05 +0600

    join page added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const fdb = require('../libs/firebase_db').fdb;
 
 function isAuthenticated(req, res, next) {
     if (!req.session.isAuthenticated) {
@@ -9,8 +9,6 @@ function isAuthenticated(req, res, next) {
     next();
 };
 
-
-
 router.get('/', isAuthenticated, (req,res,next)=>{
     console.log(req.session);
     res.render('index');
@@ -18,6 +16,13 @@ router.get('/', isAuthenticated, (req,res,next)=>{
 
 router.get('/login', (req,res)=>{
     res.render('login');
+});
+
+router.get('/join', async(req,res)=>{
+    const panel_name = req.query.panel_name;
+    // const panel = await fdb.collection('panel').where('panel_name', '==', panel_name).get();
+    console.log(panel_name)
+    res.render('join');
 });
 
 router.get('/settings', isAuthenticated, (req,res)=>{
