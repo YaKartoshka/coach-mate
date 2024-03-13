219b670158a67681cec1f0c39bbbd67c65e91786
YaKartoshka <erasyl.zarubekovv@gmail.com>
2024-03-14 00:51:20 +0600

    edit and delete news added

--- routes/news.js
+++ routes/news.js
@@ -76,17 +76,73 @@ router.post('/create', upload.single('img'), async function (req, res, next) {
     }
 });
 
-router.get('/get-all', async(req, res) => {
+router.post('/edit', upload.single('img'), async function (req, res, next) {
+    var r = { r: 1 };
+    const image = req.file;
+    const panel_id = req.session.panel_id;
+    const news_id = req.body.news_id;
+    const title = req.body.title;
+    const text = req.body.text;
+    let random = Math.floor(10000 + Math.random() * 90000);
+
+    try {
+        if (image != undefined) {
+            await storage.upload(image.path, {
+                gzip: true,
+                metadata: metadata,
+                destination: `news/${news_id + random}`
+            });
+            fs.unlink(image.path, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+
+        var data = {
+            title: title,
+            text: text,
+        }
+
+        if (image != undefined) {
+            data.news_image = `https://firebasestorage.googleapis.com/v0/b/coach-mate-b8795.appspot.com/o/news%2F${news_id + random}?alt=media`
+        }
+
+
+        await fdb.collection('panels').doc(panel_id).collection('news').doc(news_id).update(data);
+
+        res.send(JSON.stringify(r));
+    } catch (e) {
+        r['r'] = 0;
+        res.send(JSON.stringify(r))
+    }
+});
+
+router.post('/delete', async (req, res) => {
+    var r = { r: 1 };
+    let news_id = req.body.news_id;
+    const panel_id = req.session.panel_id;
+
+    await fdb.collection('panels').doc(panel_id).collection('news').doc(news_id).delete().then(() => {
+        res.send(JSON.stringify(r));
+    }).catch((e) => {
+        console.log(e);
+        r['r'] = 0;
+        res.send(JSON.stringify(r));
+    });
+});
+
+router.get('/get-all', async (req, res) => {
     const panel_id = req.session.panel_id;
     const data = [];
-    try{
-        await fdb.collection('panels').doc(panel_id).collection('news').orderBy('date', 'desc').get().then((news)=>{
-            news.forEach((news_doc)=>{
-                data.push({news_id: news_doc.id, title: news_doc.data().title, text: news_doc.data().text, image_url: news_doc.data().news_image});
+    try {
+        await fdb.collection('panels').doc(panel_id).collection('news').orderBy('date', 'desc').get().then((news) => {
+            news.forEach((news_doc) => {
+                data.push({ news_id: news_doc.id, title: news_doc.data().title, text: news_doc.data().text, image_url: news_doc.data().news_image });
             });
             res.send(JSON.stringify(data));
         });
-    } catch(e){
+    } catch (e) {
         res.send(e);
     }
 })
