177ed56ae341096729185f931b2e240b54e50f79
YaKartoshka <erasyl.zarubekovv@gmail.com>
2024-04-02 00:24:14 +0600

    members add and delete added

--- routes/events.js
+++ routes/events.js
@@ -39,7 +39,7 @@ router.post('/create', isAuthenticated, async (req, res) => {
 
 router.post('/edit', isAuthenticated, async (req, res) => {
     var r = { r: 0 };
-    const {event_id, event_name, time, week_day, coach_id, coach_name } = req.body;
+    const { event_id, event_name, time, week_day, coach_id, coach_name } = req.body;
 
     if (!week_day || !event_name || !time || !coach_id || !coach_name) {
         return res.status(400).send('All fields are required.');
@@ -63,7 +63,7 @@ router.post('/edit', isAuthenticated, async (req, res) => {
 
 router.post('/delete', isAuthenticated, async (req, res) => {
     var r = { r: 0 };
-    const {event_id } = req.body;
+    const { event_id } = req.body;
 
     if (!event_id) {
         return res.send(JSON.stringify(r));
@@ -83,9 +83,25 @@ router.get('/get', isAuthenticated, async (req, res) => {
     const panel_id = req.session.panel_id;
     const events = await fdb.collection('panels').doc(panel_id).collection('events').get();
     events.docs.forEach((event) => {
-        data.push({...event.data(), event_id: event.id});
+        data.push({ ...event.data(), event_id: event.id });
     })
     res.send(data);
+});
+
+router.post('/members/update', async (req, res) => {
+    var r = {r:0};
+    const members = req.body.members;
+    const event_id = req.body.event_id;
+
+    await fdb.collection('panels').doc(req.session.panel_id).collection('events').doc(event_id).update({
+        members: members
+    }).then(() => {
+        r['r'] = 1;
+        res.send(JSON.stringify(r));
+    }).catch((e) => {
+        console.log(e);
+        res.send(JSON.stringify(r));
+    });
 })
 
 
